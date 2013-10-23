f4055752a279885cb67bf4d5942b34344631d847
mkloubert <generalpd@gmx.de>
2013-10-24 00:03:59 +0200

    Collection helper and compiler warning
    - ADD: collection helper class with ToArray() and AsArray() methods
    - CHG: treat compiler warnings as errors

---
+++ MarcelJoachimKloubert.CLRToolbox.20/Helpers/CollectionHelper.AsArray.cs
@@ -0,0 +1,44 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Helpers
+{
+    static partial class CollectionHelper
+    {
+        #region Methods (1)
+
+        // Public Methods (1) 
+
+        /// <summary>
+        /// Converts or casts a sequence to an array.
+        /// </summary>
+        /// <typeparam name="T">Type of the items in sequence.</typeparam>
+        /// <param name="seq">The sequence to convert / cast.</param>
+        /// <returns><paramref name="seq" /> as array.</returns>
+        /// <remarks>
+        /// If <paramref name="seq" /> is <see langword="null" /> the result will also be <see langword="null" />.
+        /// If <paramref name="seq" /> is already an array, it is simply casted.
+        /// </remarks>
+        public static T[] AsArray<T>(IEnumerable<T> seq)
+        {
+            if (seq == null)
+            {
+                return null;
+            }
+
+            if (seq is T[])
+            {
+                return (T[])seq;
+            }
+
+            return ToArray<T>(seq);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolbox.20/Helpers/CollectionHelper.ToArray.cs
@@ -0,0 +1,43 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+
+using System;
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Helpers
+{
+    static partial class CollectionHelper
+    {
+        #region Methods (1)
+
+        // Public Methods (1) 
+
+        /// <summary>
+        /// Converts a sequence to an array.
+        /// </summary>
+        /// <typeparam name="T">Type of the items in sequence.</typeparam>
+        /// <param name="seq">The sequence to convert / cast.</param>
+        /// <returns><paramref name="seq" /> as array.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="seq" /> is <see langword="null" />.</exception>
+        public static T[] ToArray<T>(IEnumerable<T> seq)
+        {
+            if (seq == null)
+            {
+                throw new ArgumentNullException("seq");
+            }
+
+            List<T> list = seq as List<T>;
+            if (list == null)
+            {
+                list = new List<T>(seq);
+            }
+
+            return list.ToArray();
+        }
+
+        #endregion Methods
+    }
+}

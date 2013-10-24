f8aab6bae07b1b28f823e19f422abe738ce6dcc1
mkloubert <generalpd@gmx.de>
2013-10-25 00:09:56 +0200

    AddRange
    - ADD: extension and helper methods AddRange

---
+++ MarcelJoachimKloubert.CLRToolbox.20/Helpers/CollectionHelper.AddRange.cs
@@ -0,0 +1,49 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+using System;
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Helpers
+{
+    static partial class CollectionHelper
+    {
+        #region Methods (1)
+
+        // Public Methods (1) 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="List{T}.AddRange(IEnumerable{T})" />
+        public static void AddRange<T>(ICollection<T> coll, IEnumerable<T> items)
+        {
+            if (coll == null)
+            {
+                throw new ArgumentNullException("coll");
+            }
+
+            if (items == null)
+            {
+                throw new ArgumentNullException("items");
+            }
+
+            List<T> list = items as List<T>;
+            if (list != null)
+            {
+                list.AddRange(items);
+            }
+            else
+            {
+                foreach (T i in items)
+                {
+                    coll.Add(i);
+                }
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolbox.35/Extensions/Collections.AddRange.cs
@@ -0,0 +1,28 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+using System.Collections.Generic;
+using MarcelJoachimKloubert.CLRToolbox.Helpers;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxExtensionMethods
+    {
+        #region Methods (1)
+
+        // Public Methods (1) 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="CollectionHelper.AddRange{T}(ICollection{T}, IEnumerable{T})" />
+        public static void AddRange<T>(this ICollection<T> coll, IEnumerable<T> seq)
+        {
+            CollectionHelper.AddRange<T>(coll, seq);
+        }
+
+        #endregion Methods
+    }
+}

6a7f24b6b81f1f5bbe90ffc55f9e4f56971b2dda
mkloubert <generalpd@gmx.de>
2013-10-30 21:33:46 +0100

    extension methods
    - ADD: Randomize() extension method
    - ADD: Shuffle() extension method

---
+++ MarcelJoachimKloubert.CLRToolbox.35/Extensions/Collections.Randomize.cs
@@ -0,0 +1,65 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxExtensionMethods
+    {
+        #region Methods (2)
+
+        // Public Methods (2) 
+
+        /// <summary>
+        /// Returns the elements of a sequence in random order.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of <paramref name="seq" />.</typeparam>
+        /// <param name="seq">The input sequence.</param>
+        /// <returns>The random sequence.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> is <see langword="null" />.
+        /// </exception>
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> seq)
+        {
+            return Randomize<T>(seq, new Random());
+        }
+
+        /// <summary>
+        /// Returns the elements of a sequence in random order.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of <paramref name="seq" />.</typeparam>
+        /// <param name="seq">The input sequence.</param>
+        /// <param name="r">The random number generator to use.</param>
+        /// <returns>The random sequence.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> and/or <paramref name="r" /> are <see langword="null" />.
+        /// </exception>
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> seq, Random r)
+        {
+            if (seq == null)
+            {
+                throw new ArgumentNullException("seq");
+            }
+
+            if (r == null)
+            {
+                throw new ArgumentNullException("r");
+            }
+
+            var list = seq.ToList();
+            Shuffle<T>(list, r);
+
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolbox.35/Extensions/Collections.Shuffle.cs
@@ -0,0 +1,63 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+
+using System;
+using System.Collections.Generic;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxExtensionMethods
+    {
+        #region Methods (2)
+
+        // Public Methods (2) 
+
+        /// <summary>
+        /// Swaps the order of elements in a list.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of <paramref name="list" />.</typeparam>
+        /// <param name="list">The list that should be shuffled.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="list" /> is <see langword="null" />.
+        /// </exception>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Shuffle<T>(list, new Random());
+        }
+
+        /// <summary>
+        /// Swaps the order of elements in a list.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of <paramref name="list" />.</typeparam>
+        /// <param name="list">The list that should be shuffled.</param>
+        /// <param name="r">The random number generator to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="list" /> and/or <paramref name="r" /> are <see langword="null" />.
+        /// </exception>
+        public static void Shuffle<T>(this IList<T> list, Random r)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("list");
+            }
+
+            if (r == null)
+            {
+                throw new ArgumentNullException("r");
+            }
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                var ni = r.Next(0, list.Count);
+                var temp = list[i];
+
+                list[i] = list[ni];
+                list[ni] = temp;
+            }
+        }
+
+        #endregion Methods
+    }
+}

e2ec681febd29137e09dd101627f4fa62d6389a8
mkloubert <generalpd@gmx.de>
2014-04-27 12:43:41 +0200

    fixes and extensions for IndexOf() helper / extension methods

--- MarcelJoachimKloubert.CLRToolbox.20/Helpers/CollectionHelper.IndexOf.cs
+++ MarcelJoachimKloubert.CLRToolbox.20/Helpers/CollectionHelper.IndexOf.cs
@@ -10,9 +10,69 @@ namespace MarcelJoachimKloubert.CLRToolbox.Helpers
 {
     static partial class CollectionHelper
     {
-        #region Methods (2)
+        #region Methods (6)
 
-        // Public Methods (2) 
+        // Public Methods (6) 
+
+        /// <summary>
+        /// Returns the first zero based item of an item inside a sequence.
+        /// </summary>
+        /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
+        /// <param name="item">The item to search for.</param>
+        /// <returns>The zero based index or -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> is <see langword="null" />.
+        /// </exception>
+        public static long IndexOf(IEnumerable seq, object item)
+        {
+            return IndexOf(seq, item,
+                           delegate(object currentItem, object itemToSearch, long currentIndex)
+                           {
+                               return object.Equals(currentItem, itemToSearch);
+                           });
+        }
+
+        /// <summary>
+        /// Returns the first zero based item of an item inside a sequence.
+        /// </summary>
+        /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
+        /// <param name="item">The item to search for.</param>
+        /// <returns>The zero based index or -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> is <see langword="null" />.
+        /// </exception>
+        public static long IndexOf<T>(IEnumerable<T> seq, T item)
+        {
+            return IndexOf<T>(seq, item,
+                              EqualityComparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Returns the first zero based item of an item inside a sequence.
+        /// </summary>
+        /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
+        /// <param name="item">The item to search for.</param>
+        /// <param name="comparer">
+        /// The comparer to use to compare the current item of <paramref name="seq" />
+        /// with <paramref name="item" />.
+        /// </param>
+        /// <returns>The zero based index or -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> and/or <paramref name="comparer" /> are <see langword="null" />.
+        /// </exception>
+        public static long IndexOf(IEnumerable seq, object item, IEqualityComparer comparer)
+        {
+            if (comparer == null)
+            {
+                throw new ArgumentNullException("comparer");
+            }
+
+            return IndexOf(seq, item,
+                           delegate(object currentItem, object itemToSearch, long currentIndex)
+                           {
+                               return comparer.Equals(currentItem, itemToSearch);
+                           });
+        }
 
         /// <summary>
         /// Returns the first zero based item of an item inside a sequence.
@@ -20,12 +80,94 @@ namespace MarcelJoachimKloubert.CLRToolbox.Helpers
         /// <typeparam name="T">Type of the items of <paramref name="seq" />.</typeparam>
         /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
         /// <param name="item">The item to search for.</param>
+        /// <param name="comparer">
+        /// The comparer to use to compare the current item of <paramref name="seq" />
+        /// with <paramref name="item" />.
+        /// </param>
         /// <returns>The zero based index or -1 if not found.</returns>
-        public static long IndexOf<T>(IEnumerable<T> seq, T item)
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> and/or <paramref name="comparer" /> are <see langword="null" />.
+        /// </exception>
+        public static long IndexOf<T>(IEnumerable<T> seq, T item, IEqualityComparer<T> comparer)
+        {
+            if (comparer == null)
+            {
+                throw new ArgumentNullException("comparer");
+            }
+
+            return IndexOf<T>(seq, item,
+                              delegate(T currentItem, T itemToSearch, long currentIndex)
+                              {
+                                  return comparer.Equals(currentItem, itemToSearch);
+                              });
+        }
+
+        /// <summary>
+        /// Returns the first zero based item of an item inside a sequence.
+        /// </summary>
+        /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
+        /// <param name="item">The item to search for.</param>
+        /// <param name="finder">
+        /// The function to use to compare the current item of <paramref name="seq" />
+        /// with <paramref name="item" />.
+        /// </param>
+        /// <returns>The zero based index or -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> and/or <paramref name="finder" /> are <see langword="null" />.
+        /// </exception>
+        public static long IndexOf(IEnumerable seq, object item, Func<object, object, long, bool> finder)
         {
             if (seq == null)
             {
                 throw new ArgumentNullException("seq");
+            }
+
+            IList genList = seq as IList;
+            if (genList != null)
+            {
+                // use build-in method
+                return genList.IndexOf(item);
+            }
+
+            long index = -1;
+            foreach (object i in seq)
+            {
+                ++index;
+
+                if (finder(i, item, index))
+                {
+                    // found
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the first zero based item of an item inside a sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the items of <paramref name="seq" />.</typeparam>
+        /// <param name="seq">The sequence where to search the <paramref name="item" /> in.</param>
+        /// <param name="item">The item to search for.</param>
+        /// <param name="finder">
+        /// The function to use to compare the current item of <paramref name="seq" />
+        /// with <paramref name="item" />.
+        /// </param>
+        /// <returns>The zero based index or -1 if not found.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="seq" /> and/or <paramref name="finder" /> are <see langword="null" />.
+        /// </exception>
+        public static long IndexOf<T>(IEnumerable<T> seq, T item, Func<T, T, long, bool> finder)
+        {
+            if (seq == null)
+            {
+                throw new ArgumentNullException("seq");
+            }
+
+            if (finder == null)
+            {
+                throw new ArgumentNullException("finder");
             }
 
             IList<T> list = seq as IList<T>;
@@ -35,14 +177,12 @@ namespace MarcelJoachimKloubert.CLRToolbox.Helpers
                 return list.IndexOf(item);
             }
 
-            IList list2 = seq as IList;
-            if (list2 != null)
-            {
-                // use build-in method
-                return list2.IndexOf(item);
-            }
-
-            return -1;
+            return IndexOf((IEnumerable)seq, item,
+                           delegate(object currentItem, object i, long currentIndex)
+                           {
+                               return finder((T)currentItem, (T)i,
+                                             currentIndex);
+                           });
         }
 
         #endregion Methods
--- MarcelJoachimKloubert.CLRToolbox.35/Extensions/Collections.IndexOf.cs
+++ MarcelJoachimKloubert.CLRToolbox.35/Extensions/Collections.IndexOf.cs
@@ -3,23 +3,70 @@
 // s. http://blog.marcel-kloubert.de
 
 using MarcelJoachimKloubert.CLRToolbox.Helpers;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace MarcelJoachimKloubert.CLRToolbox.Extensions
 {
     static partial class ClrToolboxExtensionMethods
     {
-        #region Methods (1)
+        #region Methods (6)
 
-        // Public Methods (1) 
+        // Public Methods (6) 
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <see cref="CollectionHelper.IndexOf(IEnumerable, object)" />
+        public static long IndexOf(this IEnumerable seq, object item)
+        {
+            return CollectionHelper.IndexOf(seq, item);
+        }
 
         /// <summary>
         ///
         /// </summary>
         /// <see cref="CollectionHelper.IndexOf{T}(IEnumerable{T}, T)" />
-        public static long Index<T>(this IEnumerable<T> seq, T item)
+        public static long IndexOf<T>(this IEnumerable<T> seq, T item)
         {
             return CollectionHelper.IndexOf<T>(seq, item);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <see cref="CollectionHelper.IndexOf(IEnumerable, object, IEqualityComparer)" />
+        public static long IndexOf(this IEnumerable seq, object item, IEqualityComparer comparer)
+        {
+            return CollectionHelper.IndexOf(seq, item, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <see cref="CollectionHelper.IndexOf{T}(IEnumerable{T}, T, IEqualityComparer{T})" />
+        public static long IndexOf<T>(this IEnumerable<T> seq, T item, IEqualityComparer<T> comparer)
+        {
+            return CollectionHelper.IndexOf<T>(seq, item, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <see cref="CollectionHelper.IndexOf(IEnumerable, object, Func{object, object, long, bool})" />
+        public static long IndexOf(this IEnumerable seq, object item, Func<object, object, long, bool> finder)
+        {
+            return CollectionHelper.IndexOf(seq, item, finder);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <see cref="CollectionHelper.IndexOf{T}(IEnumerable{T}, T, Func{T, T, long, bool})" />
+        public static long IndexOf<T>(this IEnumerable<T> seq, T item, Func<T, T, long, bool> finder)
+        {
+            return CollectionHelper.IndexOf<T>(seq, item, finder);
         }
 
         #endregion Methods

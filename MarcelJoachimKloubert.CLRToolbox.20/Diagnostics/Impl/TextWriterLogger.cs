b36d423812b5fa85db25e6a28c27275fe2f06d1a
mkloubert <generalpd@gmx.de>
2014-05-15 11:28:06 +0200

    added factory class for TextWriterLogger

--- MarcelJoachimKloubert.CLRToolbox.20/Diagnostics/Impl/TextWriterLogger.cs
+++ MarcelJoachimKloubert.CLRToolbox.20/Diagnostics/Impl/TextWriterLogger.cs
@@ -7,10 +7,13 @@ using System.IO;
 
 namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
 {
+    #region CLASS: TextWriterLogger<W>
+
     /// <summary>
     /// A logger that is based on a <see cref="TextWriter" />.
     /// </summary>
-    public class TextWriterLogger : LoggerFacadeBase
+    /// <typeparam name="W">Type of the writer.</typeparam>
+    public class TextWriterLogger<W> : LoggerFacadeBase where W : global::System.IO.TextWriter
     {
         #region Fields (2)
 
@@ -147,21 +150,21 @@ namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
         /// </summary>
         /// <param name="logger">The underlying logger.</param>
         /// <param name="writer">The underlying writer.</param>
-        public delegate void TextWriterAction(TextWriterLogger logger, TextWriter writer);
+        public delegate void TextWriterAction(TextWriterLogger<W> logger, W writer);
 
         /// <summary>
         /// A provider that returns a <see cref="TextWriter" />.
         /// </summary>
         /// <param name="logger">The underlying logger.</param>
         /// <returns>The text writer to use.</returns>
-        public delegate TextWriter TextWriterProvider(TextWriterLogger logger);
+        public delegate W TextWriterProvider(TextWriterLogger<W> logger);
 
         #endregion Delegates and Events
 
         #region Properties (2)
 
         /// <summary>
-        /// Gets the optional disposer for the result of <see cref="TextWriterLogger.Provider" />.
+        /// Gets the optional disposer for the result of <see cref="TextWriterLogger{W}.Provider" />.
         /// </summary>
         public TextWriterAction Disposer
         {
@@ -169,7 +172,7 @@ namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
         }
 
         /// <summary>
-        /// The provider for the text writer that should be used by <see cref="TextWriterLogger.OnLog(ILogMessage)" /> method.
+        /// The provider for the text writer that should be used by <see cref="TextWriterLogger{W}.OnLog(ILogMessage)" /> method.
         /// </summary>
         public TextWriterProvider Provider
         {
@@ -178,78 +181,14 @@ namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
 
         #endregion Properties
 
-        #region Methods (5)
-
-        // Public Methods (4) 
-
-        /// <summary>
-        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
-        /// </summary>
-        /// <param name="writer">The text writer to use.</param>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="writer" /> is <see langword="null" />.
-        /// </exception>
-        public static TextWriterLogger Create(TextWriter writer)
-        {
-            return Create(writer, true);
-        }
-
-        /// <summary>
-        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
-        /// </summary>
-        /// <param name="writer">The text writer to use.</param>
-        /// <param name="syncRoot">The unique object for thread safe operations.</param>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="writer" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
-        /// </exception>
-        public static TextWriterLogger Create(TextWriter writer, object syncRoot)
-        {
-            return Create(writer, true, syncRoot);
-        }
-
-        /// <summary>
-        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
-        /// </summary>
-        /// <param name="writer">The text writer to use.</param>
-        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="writer" /> is <see langword="null" />.
-        /// </exception>
-        public static TextWriterLogger Create(TextWriter writer, bool isThreadSafe)
-        {
-            return Create(writer, isThreadSafe, new object());
-        }
-
-        /// <summary>
-        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
-        /// </summary>
-        /// <param name="writer">The text writer to use.</param>
-        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
-        /// <param name="syncRoot">The unique object for thread safe operations.</param>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="writer" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
-        /// </exception>
-        public static TextWriterLogger Create(TextWriter writer, bool isThreadSafe, object syncRoot)
-        {
-            if (writer == null)
-            {
-                throw new ArgumentNullException("writer");
-            }
-
-            return new TextWriterLogger(delegate(TextWriterLogger logger)
-                                        {
-                                            return writer;
-                                        }, null
-                                         , isThreadSafe
-                                         , syncRoot);
-        }
+        #region Methods (1)
 
         // Protected Methods (1) 
 
         /// <inheriteddoc />
         protected override void OnLog(ILogMessage msg)
         {
-            TextWriter writer = this._PROVIDER(this);
+            W writer = this._PROVIDER(this);
             if (writer == null)
             {
                 return;
@@ -270,4 +209,194 @@ namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
 
         #endregion Methods
     }
+
+    #endregion
+
+    #region CLASS: TextWriterLogger
+
+    /// <summary>
+    /// Factory class for <see cref="TextWriterLogger{W}" />.
+    /// </summary>
+    public static class TextWriterLogger
+    {
+        #region Methods (12)
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="disposer">The optional disposer for the instance of the result of <paramref name="provider" />.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, TextWriterLogger<TextWriter>.TextWriterAction disposer, bool isThreadSafe, object syncRoot)
+        {
+            return new TextWriterLogger<TextWriter>(provider, disposer, isThreadSafe, syncRoot);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, bool isThreadSafe, object syncRoot)
+        {
+            return new TextWriterLogger<TextWriter>(provider, isThreadSafe, syncRoot);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="disposer">The optional disposer for the instance of the result of <paramref name="provider" />.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, TextWriterLogger<TextWriter>.TextWriterAction disposer, bool isThreadSafe)
+        {
+            return new TextWriterLogger<TextWriter>(provider, disposer, isThreadSafe);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="disposer">The optional disposer for the instance of the result of <paramref name="provider" />.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, TextWriterLogger<TextWriter>.TextWriterAction disposer, object syncRoot)
+        {
+            return new TextWriterLogger<TextWriter>(provider, disposer, syncRoot);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="disposer">The optional disposer for the instance of the result of <paramref name="provider" />.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, TextWriterLogger<TextWriter>.TextWriterAction disposer)
+        {
+            return new TextWriterLogger<TextWriter>(provider, disposer);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, bool isThreadSafe)
+        {
+            return new TextWriterLogger<TextWriter>(provider, isThreadSafe);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider, object syncRoot)
+        {
+            return new TextWriterLogger<TextWriter>(provider, syncRoot);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <param name="provider">The function that returns the <see cref="TextWriter" /> instance that should be used for logging operation.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="provider" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<TextWriter> Create(TextWriterLogger<TextWriter>.TextWriterProvider provider)
+        {
+            return new TextWriterLogger<TextWriter>(provider);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <typeparam name="W">Type of the writer.</typeparam>
+        /// <param name="writer">The text writer to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="writer" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<W> Create<W>(W writer) where W : global::System.IO.TextWriter
+        {
+            return Create<W>(writer, true);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <typeparam name="W">Type of the writer.</typeparam>
+        /// <param name="writer">The text writer to use.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="writer" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<W> Create<W>(W writer, object syncRoot) where W : global::System.IO.TextWriter
+        {
+            return Create<W>(writer, true, syncRoot);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <typeparam name="W">Type of the writer.</typeparam>
+        /// <param name="writer">The text writer to use.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="writer" /> is <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<W> Create<W>(W writer, bool isThreadSafe) where W : global::System.IO.TextWriter
+        {
+            return Create<W>(writer, isThreadSafe, new object());
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextWriterLogger" /> class.
+        /// </summary>
+        /// <typeparam name="W">Type of the writer.</typeparam>
+        /// <param name="writer">The text writer to use.</param>
+        /// <param name="isThreadSafe">Logger should be thread safe or not.</param>
+        /// <param name="syncRoot">The unique object for thread safe operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="writer" /> and/or <paramref name="syncRoot" /> are <see langword="null" />.
+        /// </exception>
+        public static TextWriterLogger<W> Create<W>(W writer, bool isThreadSafe, object syncRoot) where W : global::System.IO.TextWriter
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            return new TextWriterLogger<W>(delegate(TextWriterLogger<W> logger)
+                                           {
+                                               return writer;
+                                           }, null
+                                            , isThreadSafe
+                                            , syncRoot);
+        }
+
+        #endregion Methods
+    }
+
+    #endregion
 }

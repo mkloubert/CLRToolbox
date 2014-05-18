bf55b91f21037cbcfff576ec5db36ad8a0be27e6
mkloubert <generalpd@gmx.de>
2014-05-18 05:38:26 +0200

    added EventLogger class

---
+++ MarcelJoachimKloubert.CLRToolbox.20/Diagnostics/Impl/EventLogger.cs
@@ -0,0 +1,49 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+using System;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics.Impl
+{
+    /// <summary>
+    /// A logger that raises an event if <see cref="EventLogger.OnLog(ILogMessage)" /> method is called.
+    /// </summary>
+    public class EventLogger : LoggerFacadeBase
+    {
+        #region Constructors (1)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventLogger" /> class.
+        /// </summary>
+        public EventLogger()
+            : base(false)
+        {
+        }
+
+        #endregion Constructors
+
+        #region Delegates and events (1)
+
+        /// <summary>
+        /// Is invoked when an <see cref="ILogMessage" /> object has been arrived in
+        /// <see cref="EventLogger.OnLog(ILogMessage)" /> method.
+        /// </summary>
+        public event EventHandler<LogMessageEventArgs> MessageReceived;
+
+        #endregion Delegates and events
+
+        #region Methods (1)
+
+        /// <inheriteddoc />
+        protected override void OnLog(ILogMessage msg)
+        {
+            if (this.RaiseEventHandler(this.MessageReceived, new LogMessageEventArgs(msg)) == false)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolbox.20/Diagnostics/LogMessageEventArgs.cs
@@ -0,0 +1,55 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. http://blog.marcel-kloubert.de
+
+using System;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Diagnostics
+{
+    /// <summary>
+    /// Arguments for an event that handles a <see cref="ILogMessage" />.
+    /// </summary>
+    public class LogMessageEventArgs : EventArgs
+    {
+        #region Fields (1)
+
+        private readonly ILogMessage _MESSAGE;
+
+        #endregion Fields
+
+        #region Constructors (1)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogMessageEventArgs" /> class.
+        /// </summary>
+        /// <param name="msg">
+        /// The value for the <see cref="LogMessageEventArgs.Message" /> property.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="msg" /> is <see langword="null" />.
+        /// </exception>
+        public LogMessageEventArgs(ILogMessage msg)
+        {
+            if (msg == null)
+            {
+                throw new ArgumentNullException("msg");
+            }
+
+            this._MESSAGE = msg;
+        }
+
+        #endregion Constructors
+
+        #region Properties (1)
+
+        /// <summary>
+        /// Gets the underlying log message.
+        /// </summary>
+        public ILogMessage Message
+        {
+            get { return this._MESSAGE; }
+        }
+
+        #endregion Properties
+    }
+}
